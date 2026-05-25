81e0c0953e47b1d0161b995c0e34d999753ebc25
amerzemour1987-debug <amerzemour1987@gmail.com>
2026-05-25 15:09:42 +0100

    Add files via upload

--- event-mate-checkin-main/event-mate-checkin-main/src/App.tsx
+++ event-mate-checkin-main/event-mate-checkin-main/src/App.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import ErrorBoundary from "@/components/ErrorBoundary.tsx";
 import Index from "./pages/Index.tsx";
 import ThankYou from "./pages/ThankYou.tsx";
 import Login from "./pages/Login.tsx";
-import ResetPassword from "./pages/ResetPassword.tsx";
 import Scanner from "./pages/Scanner.tsx";
 import Admin from "./pages/Admin.tsx";
 import AdminEvent from "./pages/AdminEvent.tsx";
@@ -17,28 +15,25 @@ import NotFound from "./pages/NotFound.tsx";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <ErrorBoundary>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/inscription/:slug" element={<Register />} />
-            <Route path="/merci" element={<ThankYou />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/scanner" element={<Scanner />} />
-            <Route path="/scanner/:id" element={<Scanner />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/admin/events/:id" element={<AdminEvent />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ErrorBoundary>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/inscription/:slug" element={<Register />} />
+          <Route path="/merci" element={<ThankYou />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/scanner" element={<Scanner />} />
+          <Route path="/scanner/:id" element={<Scanner />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/events/:id" element={<AdminEvent />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
